26612e096e100ba2ec8e131fa459cb6ed0c098b7
Sean <smirahsani@ucdavis.edu>
2024-02-26 19:32:03 -0800

    phase 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,11 +7,35 @@
 #include "disk.h"
 #include "fs.h"
 
-/* TODO: Phase 1 */
+struct Superblock
+{
+	char signature[8];	   // Signature (must be equal to "ECS150FS")
+	uint16_t total_blocks; // Total amount of blocks of virtual disk
+	uint16_t root_index;   // Root directory block index
+	uint16_t data_start;   // Data block start index
+	uint16_t data_blocks;  // Amount of data blocks
+	uint8_t fat_blocks;	   // Number of blocks for FAT
+	uint8_t padding[4079]; // Unused/Padding
+} __attribute__((packed)); // Ensure packed attribute for correct layout
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+	// Open the virtual disk file
+	if (block_disk_open(diskname) == -1)
+	{
+		fprintf(stderr, "Failed to open virtual disk file %s\n", diskname);
+		return -1;
+	}
+
+	// Read the superblock from the disk
+	struct Superblock superblock;
+	if (block_read(0, &superblock) == -1)
+	{
+		fprintf(stderr, "Failed to read superblock from disk\n");
+		return -1;
+	}
+
+	return 0;
 }
 
 int fs_umount(void)
@@ -68,4 +92,3 @@ int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
 }
-
